8e94318bd9f5c203e59748053ae7ab35eb48e371
Korneevec Ilia <korneevecin@mail.ru>
2024-03-15 09:51:47 +0300

    Auth add

---
+++ app/Models/Tokens.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tokens extends Model
+{
+    use HasFactory;
+    protected $table = "tokens";
+    protected $fillable=[
+        'acctoken'
+    ];
+    public function getToken($token)
+    {
+        $bytes = openssl_random_pseudo_bytes(20, $cstrong);
+        $accToken = bin2hex($bytes);
+        $token->acctoken = $accToken;
+        $token->save();
+    }
+}
